953d19da5a1373fd8e76a9bd1d33465765e34442
jaglorena <loremedi0103@gmail.com>
2024-04-14 01:17:17 -0600

    Componente WhoAre
    Se creó la estructura del la página "Quienes Somos"

---
+++ Proyecto/snow_style_front/src/components/pages/WhoAre/WhoAreUs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../css/Modal.css";
+
+const QuienesSomos = () => {
+  return (
+    <div className="container">
+      <header>
+        <h2>Quiénes Somos</h2>
+      </header>
+      <section className="content">
+        <p>
+          Bienvenido a SnowStyle, tu destino definitivo para la moda en la nieve
+          y el frío. Nos enorgullece presentar una selección cuidadosamente
+          curada de ropa diseñada para aquellos que aman la aventura al aire
+          libre y buscan estilo sin sacrificar funcionalidad.
+        </p>
+        <p>
+          Nuestra pasión por la moda en la nieve se refleja en cada aspecto de
+          nuestro negocio. Desde la cuidadosa selección de marcas de renombre
+          hasta nuestro compromiso con la calidad y el servicio al cliente
+          excepcional, nos esforzamos por brindarte una experiencia de compra
+          impecable en cada visita a nuestro sitio.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default QuienesSomos;
